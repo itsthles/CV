a5853358853281400b22ef2c424d07e87ce23ada
itsthles <65657229+itsthles@users.noreply.github.com>
2023-09-16 02:42:55 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-console.log("Waiting to connect!");
-// setTimeout(function () {console.log("We are online!")}, 2200);
-let count = 0;
-const myInterval = setInterval(function(){
-    console.log("We are almost there...");
-    count++;
-    if (count == 10) {
-        console.log("We are online!");
-        clearInterval(myInterval);}
-}, 1500);
-
-const codename = "Topo";
-const navbtn = document.getElementById("menu-icon");
-navbtn.addEventListener('click', () => {
-    getElementById("nav").style.display = "flex";
-});
+document.querySelector('#btn-menu').addEventListener('click', () =>{
+    document.querySelector('nav ul').classList.toggle("showMenu");
+})
